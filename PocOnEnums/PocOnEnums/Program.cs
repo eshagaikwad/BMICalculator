82d2a1dec83596f0445ca557d9901a24ecd33bc3
ESHA GAIKWAD <74804285+eshagaikwad@users.noreply.github.com>
2024-07-30 14:19:16 +0530

    Add files via upload

---
+++ PocOnEnums/PocOnEnums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PocOnEnums
+{
+    public enum Educationlevel
+    { 
+        primary,
+        secondary,
+        highSchool,
+        undergraduate,
+        Postgraduate
+    }
+    internal class Program
+    {
+        public static void PrintDetails(Student student)
+        {
+            Console.WriteLine($"Student Id: {student.Id}");
+            Console.WriteLine($"Student Name: {student.Name}");
+            Console.WriteLine($"Education Level: {student.Level}");
+        }
+        static void Main(string[] args)
+        {
+         Student student1=new Student(1,"Esha",Educationlevel.undergraduate);
+         PrintDetails(student1);
+         Console.WriteLine();
+         Student student2 = new Student(2, "Parth", Educationlevel.Postgraduate);
+         PrintDetails(student2);
+        }
+    }
+}
---
+++ PocOnEnums/PocOnEnums/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocOnEnums
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Educationlevel Level { get; set; }
+
+        public Student(int id,string name,Educationlevel level) 
+        { 
+           this.Id = id;
+            this.Name = name;
+            this.Level = level;
+        }
+
+    }
+}
