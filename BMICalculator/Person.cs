440b5c1bebd10cbae2b09d72263839d22e894441
ESHA MILIND GAIKWAD <Esha.GAIKWAD@asl.mum.sst>
2024-07-30 14:15:15 +0530

    Add project files.

---
+++ BMICalculator/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double height { get; set; }
+        public int weight { get; set; }
+
+        public double BmiStored;
+        public const double heightConstVar = 0.3048;
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            this.height = 5;
+            this.weight = 50;
+        }
+        public Person(int id, string name, int age, double height, int weight) : this(id, name, age)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.height = height;
+            this.weight = weight;
+
+        }
+        public double BMIScore(double height, int weight)
+        {
+            double heightInmeters = height * heightConstVar;
+            double bmi = weight / Math.Pow(heightInmeters, 2);
+            BmiStored = bmi;
+
+            return BmiStored;
+
+        }
+        public override string ToString()
+        {
+            return $"Person Id: {Id} \n"+
+                    $"Person name: {Name}\n"+
+                    $"Person Age: {Age}\n" +
+                    $"Person Height: {height} \n"+
+                    $"Person Weight: {weight}";
+        }
+        public string BodyType(double BMI)
+        {
+            string category=" ";
+
+            if (BMI < 18.5)
+              category=  "underweight";
+
+            else if (BMI >= 18.5 && BMI < 24.9)
+                category = "Healthy";
+
+            else if (BMI >= 24.9 && BMI < 30)
+                category="overweight";
+
+            else if (BMI >= 30)
+                category="Suffering from Obesity";
+
+            return category;
+        }
+
+    }
+}
---
+++ BMICalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    
+    internal class Program
+    {
+        static void Print(Person person)
+        {
+            Console.WriteLine(person);
+        }
+        static void Main(string[] args)
+        {
+          Person[] person = {new Person(1, "esha", 21, 5.0, 51),
+                             new Person(2, "Preet", 35, 6.7, 50),
+                             new Person(3,"Daksh",40,4.2,60),
+                             new Person(4,"Parth",22)};
+
+           foreach (Person items in person)
+           {
+                Print(items);
+                Console.WriteLine(items.BMIScore(items.height,items.weight));
+                Console.WriteLine(items.BodyType(items.BMIScore(items.height, items.weight)));
+                Console.WriteLine();
+
+           }
+
+        }
+    }
+}
